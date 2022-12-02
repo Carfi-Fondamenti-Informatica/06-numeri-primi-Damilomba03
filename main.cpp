ea2445ba59d96c915334ad18adc9c7fdda4e8d28
Damilomba03 <115529057+Damilomba03@users.noreply.github.com>
2022-12-02 18:31:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
-  return 0;
+    int x = 0, y = 2;
+    cin >> x;
+    if(nprimo(x, y) == true){
+        cout << "numero primo";
+    }
+    else{
+        cout << "numero non primo";
+    }
+    return 0;
 }
