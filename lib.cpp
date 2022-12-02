f2f1c964f2341661cbb245bc4bb1761a65e86b67
Damilomba03 <115529057+Damilomba03@users.noreply.github.com>
2022-12-02 18:28:54 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+#include "lib.h"
+bool nprimo(int x, int y){
+    if(x % y == 0){
+        if(x == y){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    nprimo(x, y + 1);
+}
